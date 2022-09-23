bfb020c9804d1a1cdaf5d261adb3dd9f3525340b
daya1765 <114075875+daya1765@users.noreply.github.com>
2022-09-23 10:53:45 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld{ //class declaration named HelloWorld
+
+public static void main(String args[]){//main method in Java
+//It is predefined method in Java
+
+System.out.println("Hello World");
+//Display statement in Java
+//It is ended with semicolon
+
+}//end of main method
+
+}//end of class
