82e70c6a28419d68f3d9295180f6eac69f5e042c
daya1765 <114075875+daya1765@users.noreply.github.com>
2022-09-24 14:59:59 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+public class Data {
+	public static void main(String[] args) {
+		GovernmentEmployee g=new GovernmentEmployee();
+		g.setEmail(129341);
+		g.setName("Amit Kumar");
+		g.setSal(40000);
+		System.out.println(g.getEmail());
+		System.out.println(g.getName());
+		System.out.println(g.getSal());
+		
+	}
+
+}
