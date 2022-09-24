ad8307339bf44c3b7de735c15435bea3b281b15e
daya1765 <114075875+daya1765@users.noreply.github.com>
2022-09-24 15:00:38 +0530

    Add files via upload

---
+++ GovernmentEmployee.java
@@ -0,0 +1,46 @@
+package CoreJava;
+
+class GovernmentEmployee {
+    private int email;
+	private String name;
+	private double sal;
+	/**
+	 * @return the email
+	 */
+	public int getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(int email) {
+		this.email = email;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the sal
+	 */
+	public double getSal() {
+		return sal;
+	}
+	/**
+	 * @param sal the sal to set
+	 */
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	
+
+}
+
