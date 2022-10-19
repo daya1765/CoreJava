a9fd896521884bf3f899f7f695d4aa8916252309
daya1765 <114075875+daya1765@users.noreply.github.com>
2022-10-19 16:00:17 +0530

    Add files via upload

---
+++ Practical/Array.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+class Array {
+
+	public static void main(String[] args) {
+		//Declaring and instantiating the array
+		int a[] = {27,0,4,7,1};
+		
+		//traversing the array
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+		
+		
+
+	}
+
+}
