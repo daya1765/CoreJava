620d4ec076a4d368d1b2bd15b7ebb4ff7485b13b
daya1765 <114075875+daya1765@users.noreply.github.com>
2022-10-19 15:19:46 +0530

    Add files via upload

---
+++ Hands-on/ArrayString.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+class ArrayString {
+
+	public static void main(String[] args) {
+		//declaring the array for string
+		String a[]=new String[3];
+		//initializing the array
+		a[0]="Raja";
+		a[1]="mango";
+		a[2]="geeta";
+		//traversing the array
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
