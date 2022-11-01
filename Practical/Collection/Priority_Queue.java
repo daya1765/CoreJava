7250e15466e7fe91f10137114c0f61c3db0ab2c5
daya1765 <114075875+daya1765@users.noreply.github.com>
2022-11-01 16:53:36 +0530

    Add files via upload

---
+++ Practical/Collection/Priority_Queue.java
@@ -0,0 +1,34 @@
+/* Priority Queue:
+ * It is a type of Queue  wherein all the elements are ordered as per their ordering 
+ * and each element is associated with a priority.
+ */
+
+package collection;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+class Priority_Queue {
+
+	public static void main(String[] args) {
+		PriorityQueue<String> num=new PriorityQueue<String>();
+		num.add("raja");
+		num.add("tina");
+		num.add("kunal");
+		System.out.println(num);
+		System.out.println("Priority Queue: ");
+		//iterator() method is used when you have to traverse a data structure
+		//hasNext() method - it evaluates as boolean value true or false
+		//next() method - it 
+		Iterator<String> itr=num.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+			
+		}
+		
+		
+		
+		
+	}
+
+}
